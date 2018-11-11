00f3487846b29301ecebfd4a6b46e3d97ad74742
Tay2druh <tay2druh@gmail.com>
2018-11-11 11:29:19 +0100

    Build fix 3

--- iworldwide/wsgi.py
+++ iworldwide/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chatter project.
+WSGI config for iworldwide project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatter.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iworldwide.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
