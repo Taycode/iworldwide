cff743897b74f319c7b0148f6238de3c37cfc765
tay2druh <tay2druh@gmail.com>
2019-04-10 02:48:02 +0100

    bug fix

--- message/urls.py
+++ message/urls.py
@@ -3,7 +3,7 @@ from message import views
 from django.contrib.auth.views import login, logout
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('/', views.index, name='home'),
     path('<str:username>/', views.message, name='message'),
     path('<str:username>/<int:message_id>)/edit/', views.editmessage, name='editmessage'),
     path('<str:username>/<int:message_id>)/delete/', views.deletemessage, name='deletemessage'),
